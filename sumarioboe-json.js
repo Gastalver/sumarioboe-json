ff7cd6d4da60b0350f53e5369a1aad51e7e92f24
Miguel Gastalver Trujillo <mgastalver@gmail.com>
2017-02-20 00:51:38 +0100

    v.0.1.0 Módulo básico

--- sumarioboe-json.js
+++ sumarioboe-json.js
@@ -1,41 +1,31 @@
 /**
  * Created by Miguel on 07/02/2017.
- * Se le da una fecha en formato YYYYMMDD y tiene que devolver el sumario del BOE de ese día en JSON
+ * Se le da una fecha en formato string YYYYMMDD y tiene que devolver el sumario del BOE de ese día en JSON
  */
 
 var httpGET = require('request'),
-    parseString = require('xml2js').parseString,
-    jsonBOE = "";
+    parseString = require('xml2js').parseString;
 
-function yyyymmdd(fecha){
+module.exports = function sumarioJSON(fecha, callback){
+    var ano,mes,dia,yyyymmdd;
     if (fecha == null) {
         fecha = new Date();
         ano = fecha.getFullYear().toString();
         mes = (fecha.getMonth() < 9 ? "0" : "") + (fecha.getMonth() + 1).toString();
         dia = (fecha.getDate() < 10 ? "0" : "") + fecha.getDate().toString();
-    } else if (typeof fecha !== 'string' || fecha.length !== 8) {
-        return ("Parámetro erróneo. Debe ser string en formato YYYYMMDD");
-    } else {
-        ano = fecha.slice(0, 4);
-        mes = fecha.slice(4, 6);
-        dia = fecha.slice(-2);
-    }
-    return ano.concat(mes, dia);
-}
+        yyyymmdd = ano.concat(mes, dia);
+    } else yyyymmdd = fecha;
 
-module.exports = function sumarioJSON(fecha, callback){
-    httpGET('http://boe.es/diario_boe/xml.php?id=BOE-S-' + yyyymmdd(fecha),
-        function (erroR, response, body) {
-        if (!erroR && response.statusCode == 200) {
+    httpGET('http://boe.es/diario_boe/xml.php?id=BOE-S-' + yyyymmdd,
+        function (error, response, body) {
+        if (!error && response.statusCode == 200) {
             parseString(body, function (err, result) {
                 callback(JSON.stringify(result));
                 });
             }
-            else {
-            callback(erroR);
-            }
+            else callback(error);
         });
-}
+};
 
 
 
